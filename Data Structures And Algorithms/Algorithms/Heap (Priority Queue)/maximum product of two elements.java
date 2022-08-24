89d9a9ae1be264935f6910fc9d0e85a31b6d7441
hiwadrashad <hiwadrashad1@gmail.com>
2022-08-24 13:26:31 +0200

    added MaxHeap

---
+++ Data Structures And Algorithms/Algorithms/Heap (Priority Queue)/maximum product of two elements.java
@@ -0,0 +1,39 @@
+//Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+ 
+
+//Example 1:
+
+//Input: nums = [3,4,5,2]
+//Output: 12 
+//Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+//Example 2:
+
+//Input: nums = [1,5,4,5]
+//Output: 16
+//Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+//Example 3:
+
+//Input: nums = [3,7]
+//Output: 12
+
+//maxheap is ordered from lowest to highest number minheap is the reverse intantiated without comperator
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < nums.length; i++)
+        {
+            for(int x = 0 ; x  < nums.length; x++)
+            {
+                if (x != i)
+                {
+                 
+                        maxHeap.add((nums[i] - 1) * (nums[x] - 1));
+                    }
+                }
+            }
+                return maxHeap.peek();
+
+        }
+    }
+// time complexity of O(N²)
