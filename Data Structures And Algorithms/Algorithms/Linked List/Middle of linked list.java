9af95a4623fd718ff16338b25bde6ac2bcb76c86
hiwadrashad <hiwadrashad1@gmail.com>
2022-09-07 11:57:50 +0200

    added linked list

---
+++ Data Structures And Algorithms/Algorithms/Linked List/Middle of linked list.java
@@ -0,0 +1,25 @@
+//given the head of a singly linked list, return the middle node of the linked list.
+
+//If there are two middle nodes, return the second middle node.
+
+ 
+
+//Example 1:
+
+
+//Input: head = [1,2,3,4,5]
+//Output: [3,4,5]
+//Explanation: The middle node of the list is node 3.
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+(
+// time compleity of O(Log N)
