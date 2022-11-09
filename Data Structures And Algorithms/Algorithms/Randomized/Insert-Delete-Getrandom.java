a720fb67d605a5485c8601dbc0b211fd97c19d52
hiwadrashad <hiwadrashad1@gmail.com>
2022-11-09 14:47:12 +0100

    Added Randomizer

---
+++ Data Structures And Algorithms/Algorithms/Randomized/Insert-Delete-Getrandom.java
@@ -0,0 +1,45 @@
+//Implement the RandomizedSet class:
+
+//RandomizedSet() Initializes the RandomizedSet object.
+//bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+//bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+//int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
+//You must implement the functions of the class such that each function works in average O(1) time complexity.
+
+ 
+    ArrayList<Integer> nums;
+    HashMap<Integer, Integer> locs;
+    java.util.Random rand = new java.util.Random();
+    
+    public RandomizedSet() {
+        nums = new ArrayList<Integer>();
+        locs = new HashMap<Integer, Integer>();
+    }
+    
+    public boolean insert(int val) {
+        boolean contain = locs.containsKey(val);
+        if ( contain ) return false;
+        locs.put( val, nums.size());
+        nums.add(val);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        boolean contain = locs.containsKey(val);
+        if ( ! contain ) return false;
+        int loc = locs.get(val);
+        if (loc < nums.size() - 1 ) {
+            int lastone = nums.get(nums.size() - 1 );
+            nums.set( loc , lastone );
+            locs.put(lastone, loc);
+        }
+        locs.remove(val);
+        nums.remove(nums.size() - 1);
+        return true;
+    }
+    
+    public int getRandom() {
+        return nums.get( rand.nextInt(nums.size()) );
+    }
+
+//time complexity of O(1)
