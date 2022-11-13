ff88df0a57d4fd1d200a64f1d6c870c63f43c8e1
hiwadrashad <hiwadrashad1@gmail.com>
2022-11-13 13:37:14 +0100

    Added Braniteaser

---
+++ Data Structures And Algorithms/Algorithms/Brainteaser/Bulb Switcher.java
@@ -0,0 +1,9 @@
+//There are n bulbs that are initially off. You first turn on all the bulbs, then you turn off every second bulb.
+
+//On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the ith round, you toggle every i bulb. For the nth round, you only toggle the last bulb.
+
+//Return the number of bulbs that are on after n rounds
+
+         return (int)Math.sqrt(n);
+
+//time complexity of O(log n)
